55fcd64c1cdf5c46eba0905ceb7a090bd7f1aadf
GoBrazill <vinicius.daraujos26@gmail.com>
2026-08-13 11:14:16 -0300

    test: add test scenarios for login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,42 @@
+describe('login', () => {
+  it('Login inválido', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+    cy.get('#email').type('email@email.com')
+    cy.get('#password').type('123')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Acesso negado! Tente novamente.').should('be.visible')
+  })
+  it('Login campos em branco', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Ei, não esqueça de digitar seu email!').should('be.visible')
+    cy.contains('Você precisa de uma senha para entrar! 🔒').should('be.visible')
+  })
+  it('Login email em branco', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#password').type('4DM')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Ei, não esqueça de digitar seu email!').should('be.visible')
+  })
+  it('Login senha em branco', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#email').type('4DM@gmail.com')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Você precisa de uma senha para entrar! 🔒').should('be.visible')
+  })
+  it('Login senha incorreta', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#email').type('4DM@gmail.com')
+    cy.get('#password').type('123')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Acesso negado! Tente novamente.').should('be.visible')
+  })
+  it('Login válido', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#email').type('4DM@gmail.com')
+    cy.get('#password').type('4DM')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Dashboard')
+  })
+})
