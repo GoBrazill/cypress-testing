d46875e891d47b47b0368323dd47911b1dd4a8ae
GoBrazill <vinicius.daraujos26@gmail.com>
2026-08-27 10:49:16 -0300

    test: finish first test case for form

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,20 +1,23 @@
 beforeEach(() => {
   cy.visit('http://localhost:3000')
+  cy.login('4DM@gmail.com', '4DM')
+  cy.contains('button', 'Formulários').should('be.visible').click()
+  cy.contains('h1', 'Consultoria').should('be.visible')
 })
 
 describe('form', () => {
-  it('passes', () => {
-    cy.login('4DM@gmail.com', '4DM')
-    cy.contains('button', 'Formulários').should('be.visible').click()
-    cy.contains('h1', 'Consultoria').should('be.visible')
+  it('Formulário preenchido corretamente', () => {
 
     cy.get('#name').type('fds vtnc')
     cy.get('#email').type('fds@gmail.com')
     cy.get('#phone').type('1')
-    
+    cy.get('#consultancyType').select('In Company')
+
     cy.contains('label', 'Pessoa Jurídica').find('input[type=radio]').click().should('be.checked')
     cy.contains('label', 'Pessoa Física').find('input[type=radio]').should('not.be.checked')
-
+    
+    cy.get('#document').type('123')
+    
     const sociais = [
       'Instagram', 'LinkedIn', 'Udemy', 'YouTube', 'Indicação de Amigo'
     ]
@@ -23,18 +26,22 @@ describe('form', () => {
       cy.contains('label', social).find('input[type=checkbox]').click().should('be.checked')
     })
 
-    cy.get('#consultancyType').select('In Company')
+    cy.get('input[type=file]').selectFile('./cypress/fixtures/apple.png', {force : true})
+    cy.contains('label', 'apple.png')
+
     
     const tecnologias = [
       'Garoto de Programa', 'DST', 'Front-end', 'Back-end', 'Banco de Dados', 'Mobile'
     ]
 
     tecnologias.forEach((tec) => {
-      cy.get('#technologies').type(tec).type('{enter}')
+      cy.get('#technologies').type(tec).type('{enter}').should('be.visible')
     })
 
     cy.contains('label', 'Li e aceito os termos de uso *').click()
 
     cy.contains('button[type=submit]', 'Enviar formulário').click()
+
+    cy.contains('h3', 'Sucesso!').should('be.visible')
   })
 })
