dfc35c26cfdb75e320cb5b6ae7184b05a51594f9
GoBrazill <vinicius.daraujos26@gmail.com>
2026-08-27 08:58:24 -0300

    test: add for each social and tecnologies section

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,4 +1,3 @@
-
 beforeEach(() => {
   cy.visit('http://localhost:3000')
 })
@@ -15,11 +14,25 @@ describe('form', () => {
     
     cy.contains('label', 'Pessoa Jurídica').find('input[type=radio]').click().should('be.checked')
     cy.contains('label', 'Pessoa Física').find('input[type=radio]').should('not.be.checked')
-    
-    cy.contains('label', 'Udemy').find('input[type=checkbox]').click().should('be.checked')
+
+    const sociais = [
+      'Instagram', 'LinkedIn', 'Udemy', 'YouTube', 'Indicação de Amigo'
+    ]
+
+    sociais.forEach((social) => {
+      cy.contains('label', social).find('input[type=checkbox]').click().should('be.checked')
+    })
 
     cy.get('#consultancyType').select('In Company')
     
+    const tecnologias = [
+      'Garoto de Programa', 'DST', 'Front-end', 'Back-end', 'Banco de Dados', 'Mobile'
+    ]
+
+    tecnologias.forEach((tec) => {
+      cy.get('#technologies').type(tec).type('{enter}')
+    })
+
     cy.contains('label', 'Li e aceito os termos de uso *').click()
 
     cy.contains('button[type=submit]', 'Enviar formulário').click()
